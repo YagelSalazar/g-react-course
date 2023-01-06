bdf3c1400e6fd1659c8a9d8015d13cb9ba10f379
YagelSalazar <yagels.xd@gmail.com>
2023-01-06 12:49:11 -0600

    first commit

---
+++ 01-birthday-reminder/setup/src/App.js
@@ -0,0 +1,16 @@
+import React, { useState } from 'react';
+import data from './data';
+import List from './List';
+function App() {
+  const [people,setPeople] = useState(data)
+  return <main>
+    <section className="container">
+        <h3> {people.length} Birthday today</h3>
+        <List personas={people}/>
+        <button onClick={()=> setPeople([])}>Delete all</button>
+    </section>
+
+  </main>
+}
+
+export default App;
